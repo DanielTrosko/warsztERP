ce88d406a0a8ff859679531cd75d8d6d50b0163e
daniel <daniel.trosko@gmail.com>
2019-09-10 19:35:16 +0200

    handleError

--- src/main/java/com/example/warszterp/controller/CustomErrorController.java
+++ src/main/java/com/example/warszterp/controller/CustomErrorController.java
@@ -4,8 +4,8 @@ import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 @Controller
 public class CustomErrorController implements ErrorController {
@@ -13,11 +13,10 @@ public class CustomErrorController implements ErrorController {
 
     @RequestMapping(value = "/error")
     public String handleError(HttpServletRequest request, Model model){
-        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
-        Exception exception = (Exception) request.getAttribute("javax.servlet.error.exception");
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        Integer statusCode = Integer.valueOf(status.toString());
         model.addAttribute("code", statusCode);
-        model.addAttribute("exc", exception.toString());
-        return "/error";
+        return "/views/error";
     }
 
 
---
+++ src/main/webapp/WEB-INF/views/error.jsp
@@ -0,0 +1,6 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<h1>
+    ${code}
+</h1>
+<h2>fgbdfgdf</h2>
